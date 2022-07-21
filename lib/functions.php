b381c4b30b26e564b01b484b7c24940245cf8b2d
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-07-21 17:01:59 -0300

    adicionada função de ler_posts_usuario() com o objetivo de tornar o código mais legível

--- lib/functions.php
+++ lib/functions.php
@@ -1,4 +1,123 @@
 <?php
+	function ler_dados_usuario($email, $pdo){
+		global $pdo;
+		$stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = '$email'");
+		 $stmt ->execute();
+			foreach($stmt as $row) {
+				global $nome;
+				global $cod;
+				global $nasc;
+				global $bio;
+				global $id;
+				global $pfp;
+				$nome = $row['nome'];
+				$nasc = $row['data_nasc'];
+				$nasc = date_create_from_format("Y-m-d", $nasc)->format("d/m/Y");
+				$cod = $row['codigo'];
+				$bio = $row['bio'];
+				$id = $row['id'];
+				$pfp = $row['profilepic'];
+				}
+			echo 
+			'<div class="card-fundo mx-auto pt-1" style="width: 50%;">
+			<div class="mx-auto pt-3 pb-3" style="width: 90%;">
+	            <div class="card card-perfil">
+	                <div class="card-body">
+	                    <div class="d-flex flex-row bd-highlight mb-0">
+	                        <div class="p-2 bd-highlight">
+	                            <img class="float-left" src="img/'.$pfp.'" width="150" height="150" title="'.$pfp.'">
+	                            <p class="mb-0" style="font-size: 18px";>
+	                                <b>Nome:</b>
+	                                <br>'.$nome.'
+	                                <a href="update-name.php?id='.$row["id"].'" class="icon-lapis">
+							            <i class="fa-solid fa-pencil"></i>
+							        </a>
+	                            </p>
+	                        </div>
+	                        <div class="p-2 bd-highlight">
+	                            <h5 class="m-0">
+	                                <b>Informações da conta:</b>
+	                            </h5>
+	                            <p class="mb-0" style="font-size: 18px";>
+	                                <b>E-mail:</b>
+	                                <br>'.$email.'
+	                            </p>
+	                            <p class="mb-0" style="font-size: 18px";>
+	                                <b>Biografia:</b>
+	                                <br>'.$bio.'
+	                                <a href="update-bio.php?id='.$row["id"].'" class="icon-lapis">
+							            <i class="fa-solid fa-pencil"></i>
+							        </a>
+	                            </p>
+	                            <p class="mb-0" style="font-size: 18px";>
+	                                <b>Data de nascimento:</b>
+	                                <br>'.$nasc.'
+	                                <a href="update-date.php?id='.$row["id"].'" class="icon-lapis">
+							            <i class="fa-solid fa-pencil"></i>
+							        </a>
+	                            </p>
+	                            <p class="mb-0" style="font-size: 18px";>
+	                                <b>Código:</b>
+	                                '.$cod.'
+	                            </p>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        </div>';
+			};
+
+    function ler_posts_usuario(){
+        global $pdo;
+        global $id;
+        global $pfp;
+        $stmt = $pdo->prepare("SELECT * FROM tbposts WHERE (usuario = '$id')  ORDER BY idpost DESC");
+        $stmt ->execute();
+        foreach ($stmt as $row) :
+        echo "<div class='mx-auto' style='width: 80%;'>
+                <!--post-->
+                <div class='mt-3 card-posts'>
+                <div class='card-body'>
+                    <div class='d-flex flex-row bd-highlight mb-0'>
+                        <div class='p-2 bd-highlight'>
+                            <img class='float-left' src='img/$pfp' width='64' height='64' title='foto'>
+                        </div>
+                        <div class='p-2 bd-highlight'>
+                            <p class='mb-0' style='font-size: 18px';>
+                                <b><u> $row[nome]</u></b>
+                                <br>
+                                <b> $row[titulo]</b>
+                            </p>
+                        </div>
+                    </div>
+                    <p class='m-1'> $row[post]</p>
+                    <div class='mx-auto m-1//' style='width: 80%;'>";
+                        if ($row['image'] != null){
+                        echo "'<img src='img/$row[image];' class='img-fluid' title='<$row[image];>";}
+                        echo "</div></div></div>";
+
+            //comentários
+            $swor = $pdo->prepare("SELECT * FROM comentarios WHERE id_post = '{$row['idpost']}'");
+            $swor->execute();
+            foreach ($swor as $swo) :
+            echo "<br>
+            <div class='d-flex flex-row bd-highlight mb-0'>
+                <div class='p-2 bd-highlight'>
+                    <img class='float-left' src='img/$pfp' width='50' height='50' title='foto'>
+                </div>
+                <div class='p-2 bd-highlight'>
+                    <p class='mb-0' style='font-size: 17px';>
+                        <b>$swo[com_nome]</b>
+                        <br>
+                        $swo[comentario]
+                    </p> </div> </div>";
+            endforeach;
+        echo "</div>";
+        endforeach;
+    echo "</div>";
+    };
+
 	function carrega_pagina_atalho($con){
 		$pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 'atalho';
 		$dir = "pags/";
@@ -275,28 +394,6 @@
 		}
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	function carrega_pagina_solicitacao($con){
 		$pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 'inicio';
 		$dir = "pags/";
@@ -502,72 +599,5 @@
 	}
 
 
-	function ler_dados_usuario($email, $pdo){
-		global $pdo;
-		$stmt = $pdo->prepare("select * from usuarios where email = '$email'");
-		 $stmt ->execute();
-			foreach($stmt as $row) {
-				global $nome;
-				global $cod;
-				global $nasc;
-				global $bio;
-				global $id;
-				global $pfp;
-				$nome = $row['nome'];
-				$nasc = $row['data_nasc'];
-				$nasc = date_create_from_format("Y-m-d", $nasc)->format("d/m/Y");
-				$cod = $row['codigo'];
-				$bio = $row['bio'];
-				$id = $row['id'];
-				$pfp = $row['profilepic'];
-				}
-			echo 
-			'<div class="card-fundo mx-auto pt-1" style="width: 50%;">
-			<div class="mx-auto pt-3 pb-3" style="width: 90%;">
-	            <div class="card card-perfil">
-	                <div class="card-body">
-	                    <div class="d-flex flex-row bd-highlight mb-0">
-	                        <div class="p-2 bd-highlight">
-	                            <img class="float-left" src="img/'.$pfp.'" width="150" height="150" title="'.$pfp.'">
-	                            <p class="mb-0" style="font-size: 18px";>
-	                                <b>Nome:</b>
-	                                <br>'.$nome.'
-	                                <a href="update-name.php?id='.$row["id"].'" class="icon-lapis">
-							            <i class="fa-solid fa-pencil"></i>
-							        </a>
-	                            </p>
-	                        </div>
-	                        <div class="p-2 bd-highlight">
-	                            <h5 class="m-0">
-	                                <b>Informações da conta:</b>
-	                            </h5>
-	                            <p class="mb-0" style="font-size: 18px";>
-	                                <b>E-mail:</b>
-	                                <br>'.$email.'
-	                            </p>
-	                            <p class="mb-0" style="font-size: 18px";>
-	                                <b>Biografia:</b>
-	                                <br>'.$bio.'
-	                                <a href="update-bio.php?id='.$row["id"].'" class="icon-lapis">
-							            <i class="fa-solid fa-pencil"></i>
-							        </a>
-	                            </p>
-	                            <p class="mb-0" style="font-size: 18px";>
-	                                <b>Data de nascimento:</b>
-	                                <br>'.$nasc.'
-	                                <a href="update-date.php?id='.$row["id"].'" class="icon-lapis">
-							            <i class="fa-solid fa-pencil"></i>
-							        </a>
-	                            </p>
-	                            <p class="mb-0" style="font-size: 18px";>
-	                                <b>Código:</b>
-	                                '.$cod.'
-	                            </p>
-	                        </div>
-	                    </div>
-	                </div>
-	            </div>
-	        </div>
-	        </div>';
-			};
+
 ?>
--- perfil.php
+++ perfil.php
@@ -20,62 +20,11 @@
 </head>
 
 <body>
-    <div><?php echo ler_dados_usuario($email, $pdo);?></div>
+    <?php ler_dados_usuario($email, $pdo);?>
 
     <div class="card-fundo mx-auto" style="width: 50%;">
         <h2 class="p-3">Meus posts</h2>
-        <?php
-        $stmt = $pdo->prepare("SELECT * FROM tbposts WHERE (usuario = '$id')  ORDER BY idpost DESC");
-        $stmt ->execute();
-        
-        foreach ($stmt as $row) :?>
-        <div class="mx-auto" style="width: 80%;">
-            <!--post-->
-            <div class="mt-3 card-posts">
-                <div class="card-body">
-                    <div class="d-flex flex-row bd-highlight mb-0">
-                        <div class="p-2 bd-highlight">
-                            <img class="float-left" src="img/<?php echo $pfp;?>" width="64" height="64" title="foto">
-                        </div>
-                        <div class="p-2 bd-highlight">
-                            <p class="mb-0" style="font-size: 18px";>
-                                <b><u><?php echo $row["nome"];?></u></b>
-                                <br>
-                                <b><?php echo $row["titulo"];?></b>
-                            </p>
-                        </div>
-                    </div>
-                    <p class="m-1"><?php echo $row["post"];?></p>
-                    <div class="mx-auto m-1//" style="width: 80%;">
-                        <?php if ($row["image"] != null){?>
-                        <img src="img/<?php echo $row["image"]; ?>" class="img-fluid" title="<?php echo $row['image']; ?>">
-                        <?php }?>
-                    </div>
-                </div>
-            </div>
-
-            <!--comentarios-->
-            <?php
-            $swor = $pdo->prepare("SELECT * FROM comentarios WHERE id_post = '{$row['idpost']}'");
-            $swor->execute();
-            foreach ($swor as $swo) : ?>
-            <br>
-            <div class="d-flex flex-row bd-highlight mb-0">
-                <div class="p-2 bd-highlight">
-                    <img class="float-left" src="img/<?php echo $pfp;?>" width="50" height="50" title="foto">
-                </div>
-                <div class="p-2 bd-highlight">
-                    <p class="mb-0" style="font-size: 17px";>
-                        <b><?php echo $swo["com_nome"];?></b>
-                        <br>
-                        <?php echo $swo["comentario"];?>
-                    </p>
-                </div>
-            </div>
-            <?php endforeach; ?>
-        </div>
-        <?php endforeach;?>
-    </div>
+        <?php ler_posts_usuario() ?>
 
 
 

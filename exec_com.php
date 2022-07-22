66e0c1944cddb9b1e36e3267e5612028a05dc6c6
HenriqueMendes7 <riquemendes745@gmail.com>
2022-07-22 18:02:23 -0300

    correção nos includes

--- exec_com.php
+++ exec_com.php
@@ -1,26 +1,22 @@
 <?php
-require 'perfil.php';
 require 'conexao.php';
-require_once 'lib/functions.php';
+require_once 'lib/includes.php';
 if($_POST["txcom"] != ""){
   $comentar = $_POST["txcom"];
   $post_id = $_POST["post_id"];
   $email_com = $_SESSION['email'];
+  $id = $_SESSION['userId'];
+  $stmt = $pdo->prepare("SELECT nome FROM usuarios WHERE id = $id");
+  $stmt -> execute();
+  foreach($stmt as $row){
+    $nome = $row['nome'];
+  }
   $stmt = $pdo->prepare("INSERT INTO comentarios(id_post, com_nome, com_user, comentario) VALUES ('$post_id', '$nome', '$id', '$comentar')");
   $stmt -> execute();
-  echo
-  "
-  <script>
-	document.location.href = 'posts.php';
-  </script>
-  ";
+  
+  header('Location: posts.php');
 }
 else{
-  echo
-  "
-  <script>
-	document.location.href = 'posts.php';
-  </script>
-  ";
+  header('Location: posts.php');
 }
 ?>

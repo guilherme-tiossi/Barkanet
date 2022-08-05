bd5f9517a5c97b59ce41ec65f46aa152391bf7ef
HenriqueMendes7 <riquemendes745@gmail.com>
2022-08-05 18:26:41 -0300

    correção na alteração de foto de perfil

--- exec_update.php
+++ exec_update.php
@@ -14,13 +14,49 @@ foreach($stmt1 as $row) {
     $stmt ->execute();
     $stmt = $pdo->prepare("UPDATE usuarios set bio='" . $_POST['bio'] . "' WHERE id='$id'");
     $stmt ->execute();
-    $stmt = $pdo->prepare("UPDATE usuarios set profilepic='" . $_POST['pfp'] . "' WHERE id='$id'");
-    $stmt ->execute();
     $stmt = $pdo->prepare("UPDATE usuarios set data_nasc='$data' WHERE id='$id'");
     $stmt ->execute();
     $stmt = $pdo->prepare("UPDATE tbposts set nome='" . $_POST['nome'] . "' WHERE usuario='$id'");
     $stmt ->execute();
     $stmt = $pdo->prepare("UPDATE comentarios set com_nome='" . $_POST['nome'] . "' WHERE com_user='$id'");
     $stmt ->execute();
+
+    $fileName = $_FILES["pfp"]["name"];
+    $fileSize = $_FILES["pfp"]["size"];
+    $tmpName = $_FILES["pfp"]["tmp_name"];
+
+    $validImageExtension = ['jpg', 'jpeg', 'png'];
+    $imageExtension = explode('.', $fileName);
+    $imageExtension = strtolower(end($imageExtension));
+    if ( !in_array($imageExtension, $validImageExtension) ){
+      echo
+      "
+      <script>
+        alert('Invalid Image Extension');
+        document.location.href = 'perfil.php';
+      </script>
+      ";
+    }
+    else if($fileSize > 10000000){
+      echo
+      "
+      <script>
+        alert('Image Size Is Too Large');
+        document.location.href = 'perfil.php';
+      </script>
+      ";
+    }
+    else{
+      $newImageName = uniqid();
+      $newImageName .= '.' . $imageExtension;
+
+      move_uploaded_file($tmpName, 'img/' . $newImageName);
+      $stmt = $pdo->prepare("UPDATE usuarios SET profilepic = '$newImageName' WHERE id = '$id'");
+      $stmt->execute();
+      $stmt = $pdo->prepare("UPDATE tbposts SET profilepic = '$newImageName' WHERE usuario = '$id'");
+      $stmt->execute();
+      header("Location: perfil.php");
+    }
+
     header("Location: perfil.php");
 ?>

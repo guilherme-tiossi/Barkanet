b02f9c913f8b7eb62dd85aa7ed65fbe7dd579324
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-08-06 11:46:48 -0300

    Início do desenvolvimento do CSS na página de posts.php

--- posts.php
+++ posts.php
@@ -3,7 +3,22 @@ include("lib/includes.php");
 include("conexao.php");
 include('menu_esquerda.php');
 include('menu_direita.php');
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Perfil</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts/font.css">
+    <script src="js/perfil.js"></script>
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+</head>
+
+<?php
 $pagina = (isset($_GET['pagina']))? $_GET['pagina'] : 1;
 
 $quantidade_pg = 50;
@@ -29,52 +44,67 @@ $incio = ($quantidade_pg*$pagina)-$quantidade_pg;
 
 $stmt = $pdo->prepare("SELECT * FROM tbposts WHERE usuario in ($users) AND idgrupo = '0' ORDER BY idpost DESC LIMIT $incio, $quantidade_pg");
 $stmt->execute();
+echo "<div class='card-fundo mx-auto' style='width: 50%;'>";
+foreach ($stmt as $row) :
+  echo "<div class='mx-auto' style='width: 80%;'>
+          <!--post-->
+          <div class='mt-3 card-posts'>
+          <div class='card-body'>
+              <div class='d-flex flex-row bd-highlight mb-0'>
+                  <div class='p-2 bd-highlight'>
+                      <img class='float-left' src='img/$pfp' width='64' height='64' title='foto'>
+                  </div>
+                  <div class='p-2 bd-highlight'>
+                      <p class='mb-0' style='font-size: 18px';>";
+                      if($row['idgrupo'] > 0){
+                          $stmt = $pdo->prepare("SELECT tbgrupos.nome_grupo from tbposts JOIN tbgrupos ON tbposts.idgrupo = tbgrupos.id_grupo WHERE usuario = '$id' AND idpost = $row[idpost]");
+                          $stmt->execute();
+                          foreach($stmt as $roww):
+                          echo "<b> $roww[nome_grupo] </b>
+                          <br>";
+                          endforeach; 	
+                          }
+                          echo "<b><u> $row[nome]</u></b>
+                          <br>
+                          <b> $row[titulo]</b>
+                      </p>
+                  </div>
+              </div>
+              <p class='m-1'> $row[post]</p>
+              <div class='mx-auto m-1//' style='width: 80%;'>";
+                  if ($row['image'] != null){
+                  echo "<img src='img/'$row[image]' class='img-fluid' title='<$row[image]';/>";}
+                  echo "</div></div>";
 
-foreach ($stmt as $row) : ?>
-  <div>
-      <img src="img/<?php echo $row["profilepic"]; ?>" width = 50 title="<?php echo $pfp; ?>">
-    <?php
-    $idposter = $row['usuario'];
-      echo "<a href='pgamigo.php?id=$idposter'>" . $row['nome'] . "</a> ";
-      echo "<br>";
-        echo $row["titulo"];
-      echo "<br>";
-      echo $row["post"];
-      echo "<br>";
-      echo "<br>";
-    if ($row["image"] != null){
-    ?>
-    <img src="img/<?php echo $row["image"]; ?>" width = 200 title="<?php echo $row['image']; ?>">
-  </div>
-    <?php }
-    $swor = $pdo->prepare("SELECT * FROM comentarios WHERE id_post = '{$row['idpost']}' ORDER BY id_com DESC");
-    $swor->execute();
-    foreach ($swor as $swo) : ?>
-  <div>
-      <img src="img/<?php echo $row["profilepic"]; ?>" width = 50 title="<?php echo $pfp; ?>">
-    <?php
-        echo $swo["com_nome"];
-        echo "<br>";
-        echo $swo["comentario"];
-      ?>
-    </div>
+      //comentários
+      $swor = $pdo->prepare("SELECT * FROM comentarios WHERE id_post = '{$row['idpost']}'");
+      $swor->execute();
+      foreach ($swor as $swo) :
+      echo "<br>
+      <div class='d-flex flex-row bd-highlight mb-0'>
+          <div class='p-2 bd-highlight'>
+              <img class='float-left' src='img/$pfp' width='50' height='50' title='foto'>
+          </div>
+          <div class='p-2 bd-highlight'>
+              <p class='mb-0' style='font-size: 17px';>
+                  <b>$swo[com_nome]</b>
+                  <br>
+                  $swo[comentario]
+              </p> </div> </div>";
+      endforeach;
+  echo '     <br>
+  <h5>Publicar seu Comentario</h5>
+  <form action="exec_com.php"  method="post">
+    <label for="txcom">Comentario:</label>
+    <input type="text" name="txcom" id="txcom" maxlength="100">
+    <span id="alert-com" class="to-hide" role="alert">Digite um comentario...</span>
     <br>
-    <?php endforeach; ?>
-    </div>
-    <br>
-    <h5>Publicar seu Comentario</h5>
-    <form action="exec_com.php"  method="post">
-      <label for="txcom">Comentario:</label>
-      <input type="text" name="txcom" id="txcom" maxlength="100">
-      <span id="alert-com" class="to-hide" role="alert">Digite um comentario...</span>
-      <br>
-      <input type="hidden" name="post_id" value="<?php echo $row["idpost"]; ?>">
-      <input type="submit" name="comentar" value="Enviar">
-    </form>
-    <?php endforeach;?>
+    <input type="hidden" name="post_id" value=';  echo $row["idpost"];  echo ' 
+    <input type="submit" name="comentar" value="Enviar">
+  </form> </div> </div>';
+  endforeach;
+  echo "</div> ";
 
-
-  <?php
     $pagina_anterior = $pagina - 1;
     $pagina_posterior = $pagina + 1;
   ?>

56d8a46391af6c381f9c88f3d758966c8ba91be9
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-09-18 13:45:12 -0300

    Aplicação da pesquisa feita por Kauã no menu esquerdo (NÃO FUNCIONAL)

--- menu_esquerda.php
+++ menu_esquerda.php
@@ -12,10 +12,89 @@ $stmt ->execute();
 <div class="card-fundo-esquerda">
   <div>
     <!--Barra de pesquisa-->
+    <form action="" method="post">
     <div class="input-group m-2 div-pesquisa">
-      <input type="text" class="form-control inputpesquisa" aria-label="Pesquisa" aria-describedby="btnpesquisa">
-      <a class="btn botaopesquisa" type="button" id="btnpesquisa" href="procurar.php"><i class="fa-solid fa-magnifying-glass fa-lg"></i></a>
-    </div>
+      <input type="text" class="form-control inputpesquisa" aria-label="Pesquisa" aria-describedby="btnpesquisa" id="live_search" autocomplete="off" placeholder="Search...">
+      <a class ="btn botaopesquisa" type="button" id="btnpesquisa"> <i class="fa-solid fa-magnifying-glass fa-lg"></i></a>   
+   </form>
+</div>
+      <div class="pesquisa" id="searchresult"></div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#live_search").keyup(function(){
+            var input = $(this).val();
+            //alert(input);
+
+            if(input !=""){
+                $.ajax({
+
+                url:"livesearch.php",
+                method:"POST",
+                data:{input:input},
+
+                success:function(data){
+                    $("#searchresult").html(data);
+                    $("#searchresult").css("display","block");
+                }
+                });
+            }else{
+                $("#searchresult").css("display","none");
+            }
+        });
+
+    });
+</script>
+
+      <!--Resultado da busca-->
+      <div>
+      <?php
+      if(isset($_POST['live_search'])){
+        $codigo = $_POST['live_search'];
+        $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE codigo = '$codigo'");
+        $stmt ->execute();
+        $count1 = $stmt->rowCount();
+        echo "Usuários: </br>";
+        if($count1 >= 0){
+          foreach($stmt as $row) {
+            $id = $row['id'];
+            $nome = $row['nome'];
+          }
+          echo "<a href='?pagina=perfil&id={$id}'>{$nome}</a> </br>";
+        }else{
+          echo "Usuário não encontrado" . "</br>";
+        }
+        
+        $stmt = $pdo->prepare("SELECT * FROM tbgrupos WHERE nome_grupo LIKE '%$codigo%'");
+        $stmt ->execute();
+        $count2 = $stmt->rowCount();
+        echo "</br>Grupos:</br>";
+          if($count2 >= 1){
+            foreach($stmt as $row) {
+              $idgrupo = $row['id_grupo'];
+              $adm_grupo = $row['adm_grupo'];
+              $nomegrupo = $row['nome_grupo'];
+
+              if($row['tipo_grupo'] == "Privado" ){
+                echo "<a href='?pagina=grupo&id_grupo=$idgrupo&id=$adm_grupo'>{$nomegrupo}</a>";
+                echo " " . $row["descricao_grupo"];
+                echo "</br>";
+              }
+
+              if($row['tipo_grupo'] == "Publico" ){
+                echo "<a href='pggrupo.php?pagina=entrar-grupo-publico&id_grupo=$idgrupo&id=$adm_grupo'>$nomegrupo</a>";
+                echo " " . $row["descricao_grupo"];
+                echo "</br>";
+              }
+            }
+          }else{
+            echo "Grupo não encontrado";
+          }
+      }
+      ?>
+      </div>
+ 
     <!--Botão editar-->
     <div class="bordaperfil m-2">
       <div class="d-flex flex-row mb-0">

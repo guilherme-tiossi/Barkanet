e2a979906a4c9c95941e58a8f771a06bd6947f22
HenriqueMendes7 <riquemendes745@gmail.com>
2022-11-03 20:07:37 -0300

    retirado lista de amigos da pagina de notificação

--- procurar.php
+++ procurar.php
@@ -16,11 +16,6 @@
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="js/jquery.mask.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $("#data").mask("00/00/0000");
-      });
-    </script>
 </head>
 
 <body>
@@ -33,40 +28,9 @@
     <!--Centro-->
     <div class="col-6">
       <div class="card-fundo-ext">
-      <?php carrega_pagina_solicitacao($con);?>
-      
-      <div>
-          <h3> Amigos </h3>
-          <?php 
-          $stmt2 = $pdo->prepare("select * from amigos where (id_de = {$_SESSION['userId']} and status = '1') or (id_para = {$_SESSION['userId']} and status = '1')");
-          $stmt2 ->execute();
-
-          foreach ($stmt2 as $row) :
-            $id_para = $row['id_para'];
-            $id_de = $row['id_de'];
-            
-            if($id_para == $_SESSION['userId']){
-              $stmt3 = $pdo->prepare("select nome from usuarios where id = '$id_de'");
-              $stmt3 ->execute();
-              foreach ($stmt3 as $row):
-                echo "<a href='?pag=1&pagina=perfil&id={$id_de}'>".$row['nome']."</a> <br>";
-                echo "<br>";
-              endforeach;
-            }
-
-            if($id_de == $_SESSION['userId']){
-              $stmt3 = $pdo->prepare("select nome from usuarios where id = '$id_para'");
-              $stmt3 ->execute();
-              foreach ($stmt3 as $row):
-                echo "<a href='?pag=1&pagina=perfil&id={$id_para}'>".$row['nome']."</a> <br>";
-                echo "<br>";
-              endforeach;
-            }
-          endforeach
-          ?>
+        <?php carrega_pagina_solicitacao($con);?>
       </div>
-      </div>
-      </div>
+    </div>
     <!--Menu Direita-->
     <div class="col">
       <?php include('menu_direita.php');?>

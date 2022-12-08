61ef75feaf45d4ee39d8038586fc7b0c8c2d849b
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-12-08 16:17:05 -0300

    CSS na página de deletado

--- deletado.php
+++ deletado.php
@@ -1,13 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-100" lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/fonts/font.css">
+	<script type="text/javascript" src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://kit.fontawesome.com/0bba2bf162.js" crossorigin="anonymous"></script>
     <title>Sentiremos sua falta!</title>
 </head>
-<body>
-    <h1> Sentiremos sua falta! </h1>
-    <a href="signup.php"> Se arrependeu? Crie outra conta! </a>
-</body>
+    <body id="intro" class="h-100 w-100">
+        <div class="row h-100 w-100 align-items-center justify-content-center">
+            <div class="col-5">
+				<div class="card text-center indexcard h-100">
+					<div id="cdheader" class="card-header d-flex gap-2">
+						<div>
+							<h5><i class="fa-solid fa-globe" style="color: #0049e6"></i></h5>
+						</div>
+						<div>
+							<h5>BARKANET</h5>
+						</div>
+					</div>
+					<div id="cdbody" class="h-100 card-body row mx-auto text-center gap-3">
+						<div class="d-flex mx-auto text-center col-auto">
+                            <div class="col-4 my-1">
+                                <img src="img/byebye.gif" alt="Tchau" class="img-fluid"/>
+                            </div>
+                            <div class="col-8 my-auto">
+							    <h4 class="card-title">Sentiremos sua falta...</h4>
+                            </div>
+						</div>
+						<div class="mx-auto my-4 col-10">
+							<a href="signup.php" class="btn my-auto btn-lg botaodeleta w-100">Se arrependeu? Crie outra conta!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+    </body>
 </html>

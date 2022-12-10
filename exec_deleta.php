7f5d7817b388c21ae4395fa6518c1090002f5ad0
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-12-10 09:41:08 -0300

    Rotina de deletar conta do usuário reativada

--- exec_deleta.php
+++ exec_deleta.php
@@ -9,7 +9,6 @@ $iduser = $_SESSION['userId'];
             foreach ($stmt as $row):
                 $idgrupo = $row["id_grupo"];
                 $nomegrupo = $row["nome_grupo"];
-				echo $idgrupo;
 				echo $_POST['40'];
 				$stmt = $pdo->prepare("UPDATE tbgrupos SET adm_grupo='" . $_POST[$idgrupo] . "' WHERE id_grupo='$idgrupo'");
 				$stmt ->execute();
@@ -21,25 +20,27 @@ $iduser = $_SESSION['userId'];
 
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+echo $email . " " . $senha;
 $stmt = $pdo->prepare("SELECT email FROM usuarios WHERE email = '$email' and senha = '$senha'");
 $stmt ->execute();
 $row = $stmt->rowCount();
 $fetch = $stmt->fetch();
 
-// if($row == 1) {
-// 	$stmt2 = $pdo->prepare("DELETE FROM usuarios WHERE usuarios.email = '$email'");
-// 	$stmt2 ->execute();
-// 	$stmt2 = $pdo->prepare("DELETE FROM tbposts WHERE usuario = '$email'");
-// 	$stmt2 ->execute();
-// 	$stmt2 = $pdo->prepare("DELETE FROM amigos WHERE (id_de = '$email') OR (id_para = '$email')");
-// 	$stmt2 ->execute();
-// 	$stmt2 = $pdo->prepare("DELETE FROM comentarios WHERE com_email = '$email'");
-// 	$stmt2 ->execute();
-// 	session_destroy();
-// 	header('Location: deletado.php');
-// 	exit();
-// } else {
-// 	$_SESSION['nao_autenticado'] = true;
-// 	header('Location: deleta.php');
-// 	exit();
-// }
+ if($row == 1) {
+  echo "bananinha";
+ 	$stmt2 = $pdo->prepare("DELETE FROM usuarios WHERE id = '$iduser'");
+ 	$stmt2 ->execute();
+ 	$stmt2 = $pdo->prepare("DELETE FROM tbposts WHERE usuario = '$iduser'");
+ 	$stmt2 ->execute();
+ 	$stmt2 = $pdo->prepare("DELETE FROM amigos WHERE (id_de = '$iduser') OR (id_para = '$iduser')");
+ 	$stmt2 ->execute();
+ 	$stmt2 = $pdo->prepare("DELETE FROM comentarios WHERE com_user = '$iduser'");
+ 	$stmt2 ->execute();
+ 	session_destroy();
+	header('Location: deletado.php');
+ 	exit();
+ } else {
+ 	$_SESSION['nao_autenticado'] = true;
+ 	header('Location: deleta.php');
+ 	exit();
+}

33c6960ddeb27eb108271d45eb48a7e009066f3d
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-12-13 15:38:54 -0300

    Início do desenvolvimento da funcionalidade de hereditariedade de grupos

--- teste.php
+++ teste.php
@@ -1,37 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
+<?php
+include "lib/includes.php";
+include "conexao.php";
+$iduser = $_SESSION['userId'];
+$stmt = $pdo->prepare("select * from tbgrupos where adm_grupo = $iduser");
+$stmt->execute();
+foreach ($stmt as $row):
+  $nome_grupo = $row['nome_grupo'];
+  $id_grupo = $row['id_grupo'];
+  
+  echo '<form> 
+  <label for="membro">Escolha um membro para ser o próximo administrador do grupo ' . $nome_grupo . ':</label>
+  <select name="membro" id="' . $id_grupo . '">';
+  $stmt = $pdo->prepare("select id_usuario, id_grupo from membros_grupos where id_grupo = 40");
+  $stmt->execute();
+  foreach ($stmt as $row):
+    $id = $row['id_usuario'];
+    $id_grupo = $row['id_grupo'];
+    echo "<option value='". $id . "'>" . $id . "</option> <br>";
+  endforeach;  
+  
+  echo '</select> </form>';
 
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  padding: 12px 16px;
-  z-index: 1;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-</style>
-</head>
-<body>
-<div class="dropdown">
-  <span>Notificações</span>
-  <div class="dropdown-content">
-  <div>Notificações</div>
-  <div>Notificações</div>
-  </div>
-</div>
-
-</body>
-</html>
+endforeach;
 
 
+
+?>
